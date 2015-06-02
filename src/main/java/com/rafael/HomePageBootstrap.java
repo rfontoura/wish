108e89cf1d06d797766edfe67127610b0d2bf333
Rafael Fontoura <rafael.fontoura@dataeasy.com.br>
2015-06-02 14:58:53 -0300

    first commit

---
+++ src/main/java/com/rafael/HomePageBootstrap.java
@@ -0,0 +1,27 @@
+package com.rafael;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+/**
+ * <b>Title:</b> HomePageBootstrap.java <br>
+ * <b>Description:</b> <br>
+ * <b>Package:</b> com.rafael <br>
+ * <b>Project:</b> wish <br>
+ * <b>Company:</b> DataEasy Consultoria e Informática LTDA. <br>
+ *
+ * Copyright (c) 2015 DataEasy - Todos os direitos reservados.
+ *
+ * @author Rafael
+ * @version Revision: $ Date: 28/05/2015
+ */
+public class HomePageBootstrap extends HomePage {
+
+    private static final long serialVersionUID = 6579686561391581830L;
+
+    /**
+     * @param parameters
+     */
+    public HomePageBootstrap(PageParameters parameters) {
+        super(parameters);
+    }
+}
